ebbef65c11bdaa70969e44ba40d3a59b8f5ce890
joao pedro <joaopedro@kinvo.com.br>
2023-07-12 00:50:38 -0300

    feat: creates commands enum

---
+++ src/common/presentation/pages/dashboard/enums/commands.ts
@@ -0,0 +1,11 @@
+export enum Commands {
+  HELLO_WORLD = 'hello world',
+  ABOUT = 'about',
+  FIND = 'find',
+  EXPERIENCE = 'experience',
+  COMMANDS = 'commands',
+  HELP = 'help',
+  THEME = 'theme',
+  LANGUAGE = 'language',
+  CLEAR = 'clear',
+}
